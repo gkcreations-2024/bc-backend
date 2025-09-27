e812c0da3073fa16f33f1d8e3fc4cdedac91acc1
gkprojects2024 <gkprojects2024@gmail.com>
2025-09-27 19:35:35 +0530

    done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const PDFDocument = require("pdfkit");
 const { Resend } = require("resend");
 const fs = require("fs");
